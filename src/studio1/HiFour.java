f6de9d0de108fdd5eefbaf5bc6d4a9b8b375ea17
fxydoctor <fxydoctor@LAPTOP-BUC949NB.dhcp.wustl.edu>
2023-09-07 10:48:58 -0500

    Studio1 	David Fan, Charlie Li, Chen wei lin

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -20,6 +20,20 @@ public class HiFour {
 		//
 		// Say hello to the names in s0 through s3.
 		//
+		System.out.print("Greetings, ");
 
+        System.out.print(s0);
+
+          System.out.print(" ");
+
+        System.out.print(s1);
+
+          System.out.print(" ");
+
+        System.out.print(s2);
+
+          System.out.print(" ");
+
+        System.out.print(s3);
 	}
 }
